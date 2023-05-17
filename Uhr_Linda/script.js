b476d957b719646efe07cbcbffb4783a69c68899
BayernGmbH <106610385+Kuttesch@users.noreply.github.com>
2023-05-17 10:47:14 +0200

    First Push
    Add:
    Uhr\index.html
    Uhr\script.js
    Uhr\styles.css
    
    Uhr_Linda\index.html
    Uhr_Linda\script.js
    Uhr_Linda\styles.css

---
+++ Uhr_Linda/script.js
@@ -0,0 +1,60 @@
+if (localStorage.getItem('darkModeEnabled') === 'true') {
+    enableDarkMode();
+  }
+  
+  function enableDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'true');
+  }
+  
+  function disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'false');
+  }
+  
+  function toggleDarkMode() {
+    if (document.body.classList.contains('dark-mode')) {
+      disableDarkMode();
+    } else {
+      enableDarkMode();
+    }
+  }
+  
+  document.getElementById('toggle-btn').addEventListener('click', toggleDarkMode);
+  
+function updateClock() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let miliseconds = now.getMilliseconds();
+
+  let digitalClock = document.getElementById('digital-clock');
+  digitalClock.textContent = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')// + ':' + miliseconds.toString().padStart(3, '0');
+
+  let hourHand = document.getElementById('hand-hour');
+  let minuteHand = document.getElementById('hand-minute');
+  let secondHand = document.getElementById('hand-sec');
+  let milsecHand = document.getElementById('hand-mili');
+
+  let degPerHour = 360 / 12;
+  let degPerMinute = 360 / 60;
+  let degPerSecond = 360 / 60;
+  let degPerMilSec = 360 / 1000;
+
+  let hourRotation = (hours % 12) * degPerHour + minutes * degPerMinute / 12;
+  let minuteRotation = minutes * degPerMinute;
+  let secondRotation = seconds * degPerSecond;
+  let milsecRotation = miliseconds * degPerMilSec;
+
+  hourHand.style.transform = /*'translate(-50%, -50%)*/' rotate(' + hourRotation + 'deg)';
+  minuteHand.style.transform = /*'translate(-50%, -50%)*/'rotate(' + minuteRotation + 'deg)';
+  secondHand.style.transform = /*'translate(-50%, -50%)*/'rotate(' + secondRotation + 'deg)';
+  milsecHand.style.transform = /*'translate(-50%, -50%)*/'rotate(' + milsecRotation + 'deg)';
+
+  setTimeout(updateClock, 1);
+}
+
+
+updateClock();
